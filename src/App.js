4f85b725bc04e4fc4889402f7737a10d3b82eea6
uzol1 <uzol12345@gmail.com>
2021-05-18 13:51:34 +0545

    updated

--- src/App.js
+++ src/App.js
@@ -2,14 +2,11 @@
 import React from 'react';
 import { Container } from 'semantic-ui-react';
 import './App.css';
-// import EventDashboard from './features/event/EventDashboard/EventDashboard';
-import NavBar from './features/nav/NavBar/NavBar';
 import Profiles from './Profiles/Profiles';
 
 function App() {
   return (
     <React.Fragment>
-      <NavBar></NavBar>
       <Container className="main">
         <Profiles></Profiles>
       </Container>
--- src/Profiles/profile/Profile.js
+++ src/Profiles/profile/Profile.js
@@ -4,138 +4,46 @@ import './Profile.css'
 
 
 const Profile = (props) => {
-    console.log(props.photos)
-    var testData = "";
+
     const [url, setUrl] = useState("");
 
-    function getImageOrFallback(url, fallback) {
-        return new Promise((resolve, reject) => {
-            const img = new Image()
-            img.src = url
-            img.onload = () => resolve(url)
-            img.onerror = () => {
-                reject(`reject`)
-            }
+    const checkImage = path =>
+        new Promise((resolve, reject) => {
+            const img = new Image();
+            img.onload = () => resolve({ path, status: "ok" });
+            img.onerror = () => resolve({ path, status: "error" });
+            img.src = path;
+        });
 
-        })
-    }
     useEffect(() => {
-        let testData = "";
+
+        var testData = "";
         for (let i = 0; i < props.photos.length; i++) {
-            getImageOrFallback(props.photos[i], "picsum.photo").then(validUrl => {
-                if (!testData) {
-                    setUrl(validUrl)
-                    testData = validUrl;
+            checkImage(props.photos[i]).then((urls) => {
+                if (urls.status === "ok" && !testData) {
+                    testData = urls.path
+                    setUrl(urls.path)
                 }
-            }).catch((msg) => { console.log("rejected") })
-
+            }).catch((error) => { })
         }
+
     }, [props.photos])
-
-    // const getImage = (props) => {
-    //     for (let i = 0; i < props.photos.length; i++) {
-    //         getImageOrFallback(props.photos[i], "https://picsum.photos/400/300").then(validUrl => {
-
-    //             console.log('valid', validUrl)
-    //             return validUrl
-
-    //         })
-
-    //     }
-    // }
-    // let img;
-    // function checkIfImageExists(url, callback) {
-    //     console.log("loop", url)
-    //     const img = new Image();
-
-    //     img.src = url;
-
-    //     img.onload = () => {
-    //         callback(true, url);
-    //     };
-    //     img.onerror = () => {
-    //         callback(false);
-    //     };
-
-    // }
-
-
-    // for (let i = 0; i < props.photos.length; i++) {
-
-    //     checkIfImageExists(props.photos[i], (exists, url) => {
-
-    //         if (!testData) {
-    //             console.log('exists', url)
-    //             testData = url;
-    //             setUrl(url)
-    //             console.log('testdata', testData)
-
-    //         }
-    //     })
-    // }
-    // for (let i = 0; i < props.photos.length; i++) {
-    //     checkIfImageExists(props.photos[i], (exists, url) => {
-    //         if (!testData) {
-    //             console.log('exists', url)
-    //             testData = url;
-    //             setUrl(url)
-
-    //         }
-    //     })
-
-    // }
 
 
     return (
-
         <div className="column-box" id="img">
             <div className="image" >
                 <div id="img">
-                    <img src={url} />
-                    {/* <img src={getImage(props)} /> */}
-
+                    <img src={url} alt={url} />
                 </div>
-                {/* <img src={testData} alt={testData} /> */}
             </div>
             <div className="text">
-                <h5 style={{ margin: "0", fontWeight: "40" }}>{props.name}</h5>
+                <h5 >{props.name}</h5>
             </div>
         </div>
-
-
     )
 
-
 }
-function checkImage(img) {
-    if (!img.complete) {
-        return false;
-    }
-    if (img.naturalWidth === 0) {
-        return false;
-    }
-    return true;
-}
-
-// function checkIfImageExists(urls) {
-//     const img = new Image();
-
-//     var errorCnt = 0;
-//     img.onLoad = () => {
-//         console.log('its working')
-//         if (!checkImage(img)) {
-//             if (errorCnt < urls.length) {
-//                 var io = urls[++errorCnt];
-//                 img.src = io;
-//             }
-//         }
-//     }
-//     img.src = urls[errorCnt];
-
-//     return img.src;
-
-// }
-
 
 
 export default Profile
